4f22456d98c7e3b97bfae601dd25ec946f66b86f
rockdong <39803354@qq.com>
2026-03-10 00:31:12 +0800

    add clean and download docker shell

---
+++ cleanDocker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 停止所有运行中的容器
+docker stop $(docker ps -aq) 2>/dev/null
+
+# 删除所有容器
+docker rm $(docker ps -aq) 2>/dev/null
+
+# 删除所有镜像
+docker rmi $(docker images -q) 2>/dev/null
+
+echo "清理完成"
+
+docker pull --platform linux/amd64 rockdong13/stock_for_cpp:v2
+
+echo "下载完成"
