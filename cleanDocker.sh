02dd8338f75fb0e30a241d9f08392663333de7bd
rockdong <39803354@qq.com>
2026-03-11 20:41:39 +0800

    改进清理脚本 - 添加删除网络功能

--- cleanDocker.sh
+++ cleanDocker.sh
@@ -9,6 +9,9 @@ docker rm $(docker ps -aq) 2>/dev/null
 # 删除所有镜像
 docker rmi $(docker images -q) 2>/dev/null
 
+# 删除所有网络
+docker network prune -f 2>/dev/null
+
 echo "清理完成"
 
 docker pull --platform linux/amd64 rockdong13/stock_for_cpp:v2
