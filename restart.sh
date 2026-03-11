9381c5da77d26f434b8731f833505920fc0d59ba
rockdong <39803354@qq.com>
2026-03-11 23:24:56 +0800

    添加 restart.sh 脚本
    - 支持一键重启所有服务
    - -a 参数启动所有服务（包括 tools）

---
+++ restart.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ALL_SERVICES=false
+
+while getopts "a" opt; do
+    case $opt in
+        a)
+            ALL_SERVICES=true
+            ;;
+        \?)
+            echo "用法: $0 [-a]"
+            echo "  -a  启动所有服务（包括 tools）"
+            exit 1
+            ;;
+    esac
+done
+
+echo "停止服务..."
+docker-compose down
+
+echo "启动服务..."
+if [ "$ALL_SERVICES" = true ]; then
+    echo "启动所有服务（包括 tools）..."
+    docker-compose --profile tools up -d
+else
+    echo "启动主服务..."
+    docker-compose up -d
+fi
+
+echo "完成！"
+docker-compose ps
