368da74d67675a5e3717a883b10eb0abbbc3f225
rockdong <39803354@qq.com>
2026-03-21 22:49:00 +0800

    chore: restart.sh 启动后打印服务访问地址
    启动完成后显示：
    - 主服务: http://localhost:3000
    - 数据库: http://localhost:8080
    - 日志监控: http://localhost:8888

--- restart.sh
+++ restart.sh
@@ -6,5 +6,14 @@ docker-compose down
 echo "启动所有服务..."
 docker-compose --profile tools --profile db up -d
 
-echo "完成！"
+echo ""
+echo "========================================"
+echo "服务启动完成！"
+echo "========================================"
 docker-compose ps
+echo ""
+echo "访问地址："
+echo "  主服务:     http://localhost:3000"
+echo "  数据库:     http://localhost:8080"
+echo "  日志监控:   http://localhost:8888"
+echo "========================================"
